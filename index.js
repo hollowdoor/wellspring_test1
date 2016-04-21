431d9f80f93335b266d34637d81678d2b556148e
hollowdoor <hollowdoor99@gmail.com>
2016-04-21 13:31:11 -0500

    Everything is fine.

---
+++ index.js
@@ -0,0 +1,13 @@
+/*
+git remote add origin https://github.com/hollowdoor/wellspring_test1.git
+git push -u origin master
+*/
+export default class WellSpringTest1 {
+
+    ready(){
+        console.log('MyPlugin is ready')
+    }
+    closed(){
+        console.log('MyPlugin is closing')
+    }
+}
